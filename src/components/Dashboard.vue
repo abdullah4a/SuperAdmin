dbff8ed4fe5c8bad24f4370f60b34abd70c1bfe2
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-09-10 00:03:21 +0500

    Update Dashboard.vue

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -4,6 +4,21 @@
       <v-card-title>
         {{ ClassName }}
       </v-card-title>
+      <v-card-content>
+        <v-data-table
+          :headers="headers"
+          :items="desserts"
+          :items-per-page="2"
+          item-key="name"
+          class="elevation-3"
+          :footer-props="{
+            showFirstLastPage: true,
+            prevIcon: 'mdi-minus',
+            nextIcon: 'mdi-plus',
+          }"
+        >
+        </v-data-table>
+      </v-card-content>
     </v-card>
   </v-app>
 </template>
@@ -14,5 +29,31 @@ import Component from "vue-class-component";
 @Component({})
 export default class Dashboard extends Vue {
   private ClassName = "DashBoard";
+  private headers = [
+    {
+      text: "Item (1Kg serving)",
+      align: "start",
+      value: "name",
+    },
+    { text: "Price", value: "price" },
+    { text: "In Stock", value: "Availability" },
+    { text: "Remarks", value: "remarks" },
+  ];
+  private desserts = [
+    {
+      name: "Frozen Yogurt",
+      price: "100",
+      Availability:"Available"
+    },
+    {
+      name: "Ice cream sandwich",
+      price: "98.44",
+      Availability:"Not Avail"
+    },
+    {
+      name: "Eclair",
+      price: "5",
+    },
+  ];
 }
 </script>
