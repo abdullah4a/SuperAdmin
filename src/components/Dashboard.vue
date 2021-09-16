7c31b490f516bf879bb6dd58aca59603e362aafa
abdullah4a <45167847+abdullah4a@users.noreply.github.com>
2021-09-16 23:59:08 +0500

    Update Dashboard.vue

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -98,10 +98,10 @@
                 nextIcon: 'mdi-plus',
               }"
             >
-              <!-- sort-by="name" -->
-              <!-- <template #item.remarks="{ item }">
-              <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
-            </template> -->
+              sort-by="name" -->
+              <template #item.remarks="{ item }">
+                <v-icon small @click="deleteItem(item)">mdi-delete</v-icon>
+              </template>
             </v-data-table>
           </v-col>
         </v-row>
